6c90e70f5a770a6644d28072427d172cdd325978
komix <komixdbz@gmail.com>
2017-11-12 23:03:56 +0200

    final ui-changes

--- app/core/base.controller.js
+++ app/core/base.controller.js
@@ -5,54 +5,25 @@
         .module('app')
         .controller('BaseController', BaseController);
 
-    BaseController.$inject = ['$state', 'users', '$rootScope', 'routerHelper'];
+    BaseController.$inject = ['$state', 'users', '$rootScope'];
     /* @ngInject */
-    function BaseController($state, users, $rootScope, routerHelper) {
+    function BaseController($state, users, $rootScope) {
         var vm = this;
 
         vm.users = users;
         vm.states = [];
-        vm.isStateAvailable = isStateAvailable;
-        vm.logout = logout;
-        vm.isLoggedIn = isLoggedIn;
-        vm.isStateActive = isStateActive;
-        vm.getStateHref = getStateHref;
         vm.isActiveStateWide = isActiveStateWide;
         //vm.isActiveStateFullHeight = isActiveStateFullHeight;
 
         activate();
 
         function activate() {
-            updateStates();
             $rootScope.$on('token-invalid', function(){
                 users.logout();
             });
-
-            $rootScope.$on('user-logged-in', function(){
-                updateStates();
-            });
         }
 
-        function logout() {
-            users.logout();
-        }
 
-        function isStateAvailable(stateName) {
-            return routerHelper.isStateAvailable(stateName);
-        }
-
-        function isLoggedIn() {
-            return users.current && users.current.role !== 'guest';
-        }
-
-        function isStateActive(name) {
-            if (!$state.current || !$state.current.name) { return false; }
-            return $state.current.name.includes(name);
-        }
-
-        function getStateHref(state) {
-            return $state.href(state.name, state.params);
-        }
 
         function isActiveStateWide() {
             var wideStates = [
@@ -90,47 +61,7 @@
         //    return _.indexOf(highStates, $state.current.name) !== -1;
         //}
 
-        function updateStates() {
-            var statesList = [
-                {
-                    ukName: 'Головна',
-                    name: 'home'
-                },
-                {
-                    ukName: 'Про проект',
-                    name: 'about'
-                },
-                {
-                    ukName: 'Секції',
-                    name: 'institutions.all-state'
-                },
-                {
-                    ukName: 'Пошук',
-                    name: 'search.default-state'
-                },
-                {
-                    ukName: 'Профіль',
-                    name: 'profile',
-                    params: {
-                        id: users.current ? users.current.id : null
-                    }
-                },
-                {
-                    ukName: 'Новини',
-                    name: 'news.news-list'
-                },
-                {
-                    ukName: 'Адмінка',
-                    name: 'dashboard'
-                }
-            ];
 
-            vm.states.length = 0;
-
-            _.each(statesList, function(elem) {
-                vm.states.push(elem);
-            })
-        }
     }
 })();
 
